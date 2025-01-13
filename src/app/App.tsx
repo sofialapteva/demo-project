d1233086d2edf30d91f4fcdb492e87dea73cd684
s.lapteva <s.lapteva@kapitalbank.uz>
2025-01-14 01:17:13 +0300

    chore/introduce-fsd

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,10 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
 import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { useTheme } from "./providers/ThemeProvider";
 import "./styles/index.scss";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
-interface AppProps {
-  className?: string;
-}
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -19,8 +16,8 @@ export const App = () => {
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={"/about"} element={<AboutPageAsync />} />
-          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPage />} />
+          <Route path={"/"} element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
